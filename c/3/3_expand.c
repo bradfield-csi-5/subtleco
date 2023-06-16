f0e3d4c1b9d5a882d65af7af8c7d63decf4cf4c1
Alex Martin <alex.martin@getbuilt.com>
2023-06-16 07:52:41 -0500

    3_3 done

--- c/3/3_expand.c
+++ c/3/3_expand.c
@@ -1,36 +1,65 @@
 #include <stdio.h>
 #include <string.h>
-#define NUMBER 0
-#define LETTER 1
 
 void expand(char[]);
 
 int main() {
   expand("a-z");
+  putchar('\n');
+  expand("0-8");
+  putchar('\n');
+  expand("a-f-k");
+  putchar('\n');
+  expand("1-4-7");
+  putchar('\n');
+  expand("-a-z-");
   return 0;
 }
 
+// prev means a character has been seen
+// start means a character and a - has been seen
+
 void expand(char input[]) {
-  int i, prev, start, type;
-  char c, j;
+  int i, prev = 0, start = 0;
+  char c, j, last;
+
   for (i = 0; i < strlen(input); i++) {
     c = input[i];
+
     if (c >= 'a' && c <= 'z') {
-      type = LETTER;
       prev = 1;
       if (start) {
         for (j = start; j <= c; j++) {
-          putchar(j);
+          if (j != last) {
+            putchar(j);
+            last = j;
+          }
         }
+        start = 0;
       }
     }
 
     if (c >= '0' && c <= '9') {
-      type = NUMBER;
+      prev = 1;
+      if (start) {
+        for (j = start; j <= c; j++) {
+          if (j != last) {
+            putchar(j);
+            last = j;
+          }
+        }
+        start = 0;
+      }
     }
 
-    if (c == '-' && prev) {
-      start = input[i - 1];
+    if (c == '-') {
+      if (prev) {
+        start = input[i - 1];
+        if (!input[i + 1]) {
+          putchar(c);
+        }
+      } else
+        putchar(c);
     }
   }
 }
