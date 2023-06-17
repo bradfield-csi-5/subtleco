8fa5abf5246bc20d69765414c8d672d54ecf8593
Alex Martin <alex.martin@getbuilt.com>
2023-06-17 08:20:37 -0500

    correct 2_6

--- c/2/6_setbits.c
+++ c/2/6_setbits.c
@@ -11,16 +11,20 @@ int main() {
 // Write a function that returns x with the n bits that begin at p set to the
 // right most n bits of y
 int setbits(int x, int p, int n, int y) {
-  int template, x_adj, y_adj, result;
+  int x_template = 0, y_template = 0, x_adj = 0, y_adj = 0, result = 0;
+
+  // Create cookie cutter template to remove bits from x
   while (n > 0) {
-    template += pow(2,p);
+    x_template += pow(2, p);
+    y_template += pow(2, n - 1);
     p--;
     n--;
   }
-  x_adj = x & ~template;
-  printf("x_adj = %d\n", x_adj);
-  y_adj = y & template;
-  printf("y_adj = %d\n", y_adj);
+  printf("x_template = %d\n", x_template);
+  printf("y_template = %d\n", y_template);
+  x_adj = x & ~x_template;
+  y_adj = y & y_template;
+  y_adj <<= (p + 1);
   result = x_adj | y_adj;
   return result;
 }
