de32cae10237cdbe19c98c2a1a029b6afec49639
Alex Martin <alexmartin@Alexs-MacBook-Pro.local>
2023-07-08 22:39:48 -0500

    7_1

---
+++ c/7/1_upper_lower.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+  int c;
+  
+  
+  if (strcmp(argv[0], "./lower") == 0)
+    while ((c = getchar()) != EOF)
+      putchar(tolower(c));
+  if (strcmp(argv[0], "./upper") == 0)
+    while ((c = getchar()) != EOF)
+      putchar(toupper(c));
+  return 0;
+}
