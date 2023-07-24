e3bb9ed61fd11b366e7365a03849abbb74643eef
Alex Martin <raymond.alex.martin@gmail.com>
2023-07-24 11:22:13 -0500

    structs with methods

--- go/go_with_tests/arrays/sum_test.go
+++ go/go_with_tests/arrays/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -15,4 +18,23 @@ func TestSum(t *testing.T) {
 		}
 	})
 
+}
+
+func TestSumAll(t *testing.T) {
+
+  got := SumAll([]int{1,2,3}, []int{4,5,6,7})
+  want := []int{6,22}
+
+  if !reflect.DeepEqual(got, want) {
+    t.Errorf("got %v want %v", got, want)
+  }
+}
+
+func TestSumAllTails(t *testing.T) {
+	got := SumAllTails([]int{1, 2}, []int{0, 9})
+	want := []int{2, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
---
+++ go/go_with_tests/structs_methods_interfaces/shape_test.go
@@ -0,0 +1,37 @@
+package shapes
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+  rect := Rectangle{10.0, 10.0}
+	got := rect.Perimeter()
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+
+	t.Run("rectangles", func(t *testing.T) {
+		rect := Rectangle{12, 6}
+		got := rect.Area()
+		want := 72.0
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+
+}
