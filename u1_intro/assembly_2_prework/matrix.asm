28b4aed6c933d978ad1826bb31ad4c8eae9e8ab3
Alex Martin <raymond.alex.martin@gmail.com>
2023-08-17 11:59:56 -0500

    use only r size registers

--- u1_intro/assembly_2_prework/matrix.asm
+++ u1_intro/assembly_2_prework/matrix.asm
@@ -9,5 +9,5 @@ index:
   imul rcx, rdx     ; skip_row_elements = r_index * cols
   add rcx, r8       ; c_index can be taken literally. Add skip_row_index to c_index to get total skip value
   imul rcx, 4       ; multiply skip value by size of int in bytes
-  mov eax, rcx[rdi] ; dereference matrix with skip value size added to address
+  mov rax, rcx[rdi] ; dereference matrix with skip value size added to address
 	ret               ; gimme
