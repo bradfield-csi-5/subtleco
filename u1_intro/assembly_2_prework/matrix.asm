fa29700d6f95b5b770f5e7f8f3b27f6ce66e6ce0
Alex Martin <raymond.alex.martin@gmail.com>
2023-08-17 11:58:26 -0500

    matrix with notes

--- u1_intro/assembly_2_prework/matrix.asm
+++ u1_intro/assembly_2_prework/matrix.asm
@@ -4,6 +4,10 @@ index:
 	; rdi: matrix
 	; rsi: rows
 	; rdx: cols
-	; rcx: rindex
-	; r8: cindex
-	ret
+	; rcx: r_index
+	; r8:  c_index
+  imul rcx, rdx     ; skip_row_elements = r_index * cols
+  add rcx, r8       ; c_index can be taken literally. Add skip_row_index to c_index to get total skip value
+  imul rcx, 4       ; multiply skip value by size of int in bytes
+  mov eax, rcx[rdi] ; dereference matrix with skip value size added to address
+	ret               ; gimme
