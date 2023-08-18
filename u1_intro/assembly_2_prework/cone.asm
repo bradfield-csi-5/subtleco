20a6a6539e1cc744a1c7a9882ad5a9605c4705f8
Alex Martin <raymond.alex.martin@gmail.com>
2023-08-17 19:09:00 -0500

    finish cone

--- u1_intro/assembly_2_prework/cone.asm
+++ u1_intro/assembly_2_prework/cone.asm
@@ -1,6 +1,25 @@
 default rel
 
+; v = pi * r * r * (h/3)
 section .text
 global volume
+
+; xmm0 = r
+; xmm1 = h
+
 volume:
- 	ret
+  movss xmm2, [pi]
+  movss xmm3, [three]
+  mulss xmm0, xmm0 ; square the radius
+  divss xmm1, xmm3
+  mulss xmm0, xmm1
+  mulss xmm0, xmm2
+
+  ret
+
+
+section .rodata
+  pi:
+    dd 3.14159265
+  three:
+    dd 3.00000000
