959fbf0bd0d3b51f5f5b471aa83e8f27f5b65611
Alex Martin <raymond.alex.martin@gmail.com>
2023-09-17 20:52:16 -0500

    parse IP Datagrams

--- u2_network/pcapture/pcap.go
+++ u2_network/pcapture/pcap.go
@@ -25,7 +25,7 @@ type PcapFile struct {
 type PcapPacket struct {
 	TimeStampSeconds      uint32
 	TimeStampMicroseconds uint32
-	PacketLenght          uint32
+	PacketLength          uint32
 	FullPacketLength      uint32
 	Payload               []byte
 }
@@ -35,6 +35,23 @@ type EthernetFrame struct {
 	MacSource []byte
 	EthType   uint16
 	Payload   []byte
+}
+
+type IPDatagram struct {
+	Version        uint8
+	IHL            uint8
+	DSCP           uint8
+	ECN            uint8
+	TotalLength    uint16
+	Identification uint16
+	Flags          uint8
+	FragmentOffset uint16
+	TTL            uint8
+	Protocol       uint8
+	HeaderChecksum uint16
+	SourceIP       uint32
+	DestinationIP  uint32
+	Payload        []byte
 }
 
 func check(e error) {
@@ -60,13 +77,13 @@ func main() {
 	// Split into Pcap Packets
 	file_payload_size := file_size - PCAP_FILE_HEADER
 	pcap_packets := splitPcapPackets(pcap_file.Payload, file_payload_size)
-	fmt.Println(len(pcap_packets))
 
 	// Split into Ethernet Frames
 	eth_frames := parseEthernetFrames(pcap_packets)
-	fmt.Println(len(eth_frames))
 
 	// Parse IP Datagrams
+	ip_datagrams := parseIPDatagrams(eth_frames)
+	fmt.Println(len(ip_datagrams))
 
 	// Parse TCP Segments
 
@@ -96,7 +113,7 @@ func splitPcapPackets(packets []byte, file_size int) []PcapPacket {
 		pcap_packet := PcapPacket{
 			TimeStampSeconds:      binary.LittleEndian.Uint32(packets[packet_start : packet_start+4]),
 			TimeStampMicroseconds: binary.LittleEndian.Uint32(packets[packet_start+4 : packet_start+8]),
-			PacketLenght:          packet_length,
+			PacketLength:          packet_length,
 			FullPacketLength:      binary.LittleEndian.Uint32(packets[packet_start+12 : packet_start+16]),
 			Payload:               packets[packet_start+16 : packet_start+16+int(packet_length)],
 		}
@@ -115,10 +132,38 @@ func parseEthernetFrames(packets []PcapPacket) []EthernetFrame {
 		eth_frame := EthernetFrame{
 			MacDest:   payload[:6],
 			MacSource: payload[6:12],
-			EthType:   binary.LittleEndian.Uint16(payload[12:16]),
+			EthType:   binary.BigEndian.Uint16(payload[12:14]), // all IPv4 (0x0800)
 			Payload:   payload[14:],
 		}
 		all_frames = append(all_frames, eth_frame)
 	}
 	return all_frames
+}
+
+func parseIPDatagrams(eth_frames []EthernetFrame) []IPDatagram {
+	all_datagrams := make([]IPDatagram, 0)
+	for _, frame := range eth_frames {
+		payload := frame.Payload
+		datagram := IPDatagram{
+			Version:        payload[0] >> 4,
+			IHL:            payload[0] & 0x0f,
+			DSCP:           payload[1] >> 2,
+			ECN:            payload[1] & 0x03,
+			TotalLength:    binary.BigEndian.Uint16(payload[2:4]),
+			Identification: binary.BigEndian.Uint16(payload[4:6]),
+			Flags:          payload[6] >> 5,
+			FragmentOffset: binary.BigEndian.Uint16(payload[6:8]),
+			TTL:            payload[8],
+			Protocol:       payload[9],
+			HeaderChecksum: binary.BigEndian.Uint16(payload[10:12]),
+			SourceIP:       binary.BigEndian.Uint32(payload[12:16]),
+			DestinationIP:  binary.BigEndian.Uint32(payload[16:20]),
+		}
+		if datagram.IHL > 5 {
+			datagram.Payload = payload[20:]
+		}
+		all_datagrams = append(all_datagrams, datagram)
+		fmt.Println(datagram.DestinationIP)
+	}
+	return all_datagrams
 }
