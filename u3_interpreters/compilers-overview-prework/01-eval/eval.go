d8b06615e874a0098740bb7f6a5da1074959a703
Alex Martin <raymond.alex.martin@gmail.com>
2023-10-23 14:16:00 -0500

    passing 1 & 2

--- u3_interpreters/compilers-overview-prework/01-eval/eval.go
+++ u3_interpreters/compilers-overview-prework/01-eval/eval.go
@@ -1,17 +1,51 @@
 package main
 
 import (
+	"fmt"
 	"go/ast"
 	"go/parser"
 	"go/token"
 	"log"
+	"strconv"
 )
 
 // Given an expression containing only int types, evaluate
 // the expression and return the result.
 func Evaluate(expr ast.Expr) (int, error) {
-	// TODO
+	switch xT := expr.(type) {
+
+	case *ast.BasicLit:
+		return strconv.Atoi(xT.Value)
+
+	case *ast.BinaryExpr:
+		x, _ := Evaluate(xT.X)
+		y, _ := Evaluate(xT.Y)
+		op := xT.Op
+		return arith(x, y, op)
+
+	case *ast.ParenExpr:
+		x, _ := Evaluate(xT.X)
+		return x, nil
+
+	default:
+		fset := token.NewFileSet()
+		_ = ast.Print(fset, xT)
+	}
 	return 0, nil
+}
+
+func arith(x, y int, op token.Token) (int, error) {
+	switch op {
+	case token.ADD:
+		return x + y, nil
+	case token.SUB:
+		return x - y, nil
+	case token.MUL:
+		return x * y, nil
+	default:
+		fmt.Println("op is ", op)
+		return 0, nil
+	}
 }
 
 func main() {
--- u3_interpreters/compilers-overview-prework/02-refactor/refactor.go
+++ u3_interpreters/compilers-overview-prework/02-refactor/refactor.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"log"
 	"os"
+	"sort"
 
 	"github.com/dave/dst"
 	"github.com/dave/dst/decorator"
@@ -30,8 +32,44 @@ func bar() {
 // Moves all top-level functions to the end, sorted in alphabetical order.
 // The "source file" is given as a string (rather than e.g. a filename).
 func SortFunctions(src string) (string, error) {
-	// TODO
-	return src, nil
+	f, err := decorator.Parse(src)
+	if err != nil {
+		panic(err)
+	}
+
+	var declarations []*dst.GenDecl
+	var functions []*dst.FuncDecl
+
+	// Map out AST
+	for _, node := range f.Decls {
+		switch nodeT := node.(type) {
+		case *dst.FuncDecl:
+			functions = append(functions, nodeT)
+		case *dst.GenDecl:
+			declarations = append(declarations, nodeT)
+		}
+	}
+
+	// Sort SortFunctions
+	sort.Slice(functions, func(i, j int) bool {
+		return functions[i].Name.Name < functions[j].Name.Name
+	})
+
+	// Assign statements
+	for i := range f.Decls {
+		if i < len(declarations) {
+			f.Decls[i] = declarations[i]
+		} else {
+			pos := i - len(declarations)
+			f.Decls[i] = functions[pos]
+		}
+	}
+
+	var buf bytes.Buffer
+	if err := decorator.Fprint(&buf, f); err != nil {
+		return "", err
+	}
+	return buf.String(), nil
 }
 
 func main() {
